454c66f5263bfd0a36f6623fa134a9975d6fa95f
ColdCrawd <edgar.r.0228@gmail.com>
2026-03-25 16:32:01 +0000

    Ahroa funciona, y emepce session

---
+++ onnx/main.cpp
@@ -0,0 +1,11 @@
+#include <onnxruntime_cxx_api.h>
+
+Ort::Env env (ORT_LOGGING_LEVEL_WARNING, "MyApp");
+
+int main(){
+
+	Ort::SessionOptions session_options;
+	Ort::Session session()
+
+	return 0;
+}
